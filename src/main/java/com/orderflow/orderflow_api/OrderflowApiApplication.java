2e3a4ef9cb06c32a75673a55fc92c69d5816222b
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-12-21 22:19:07 -0300

    feat: initialized api project

---
+++ src/main/java/com/orderflow/orderflow_api/OrderflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.orderflow.orderflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderflow/orderflow_api/OrderflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderflow.orderflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
