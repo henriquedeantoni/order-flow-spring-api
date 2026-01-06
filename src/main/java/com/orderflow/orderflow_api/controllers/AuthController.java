437cdc569bb36288eff29732d8bb56003eae772d
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-01-06 13:49:21 -0300

    feat: jwt utils methods and auth controller user info recovery information

--- src/main/java/com/orderflow/orderflow_api/controllers/AuthController.java
+++ src/main/java/com/orderflow/orderflow_api/controllers/AuthController.java
@@ -22,10 +22,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -131,5 +128,27 @@ public class AuthController {
         user.setRoles(roles);
         userRepository.save(user);
         return ResponseEntity.ok("User registered successfully");
+    }
+
+    @GetMapping("/username")
+    public String getUsername(Authentication authentication){
+        if(authentication != null){
+            return authentication.getName();
+        } else {
+            return "";
+        }
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<?> getUser(Authentication authentication){
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        List<String> roles = userDetails.getAuthorities()
+                .stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+        UserInfoResponse userResponse = new UserInfoResponse(userDetails.getId(), userDetails.getUsername(), roles);
+
+        return ResponseEntity.ok().body(userResponse);
     }
 }
---
+++ src/main/java/com/orderflow/orderflow_api/secutiry/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.orderflow.orderflow_api.secutiry.response;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
