6d11f96bf5338cb853b6092a985a7560ac87550e
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-01-29 23:30:26 -0300

    refact: decouple the controller logic from service layer

--- src/main/java/com/orderflow/orderflow_api/services/AuthService.java
+++ src/main/java/com/orderflow/orderflow_api/services/AuthService.java
@@ -4,10 +4,17 @@ import com.orderflow.orderflow_api.payload.AuthenticationResult;
 import com.orderflow.orderflow_api.secutiry.request.LoginRequest;
 import com.orderflow.orderflow_api.secutiry.request.SignupRequest;
 import com.orderflow.orderflow_api.secutiry.response.MessageResponse;
+import com.orderflow.orderflow_api.secutiry.response.UserInfoResponse;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 
 public interface AuthService {
     AuthenticationResult login(LoginRequest request);
 
     public ResponseEntity<MessageResponse> register(SignupRequest request);
+
+    UserInfoResponse getUserDetails(Authentication authentication);
+
+    ResponseCookie logOutUser();
 }
