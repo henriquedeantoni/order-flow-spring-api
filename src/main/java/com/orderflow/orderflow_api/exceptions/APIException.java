55563746a1a1dec7114ab1ce5227c0fb08ef4e85
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-02-04 00:33:46 -0300

    refact: api exception extends runtime exception

--- src/main/java/com/orderflow/orderflow_api/exceptions/APIException.java
+++ src/main/java/com/orderflow/orderflow_api/exceptions/APIException.java
@@ -1,6 +1,6 @@
 package com.orderflow.orderflow_api.exceptions;
 
-public class APIException  extends Exception{
+public class APIException  extends RuntimeException{
     private static final long serialVersionUID = 1L;
 
     public APIException(String message)
