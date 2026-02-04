5b606b16ad62f1ed3429fe5f974d1e9bfd99fd5f
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-02-04 00:32:59 -0300

    feat: category payload

---
+++ src/main/java/com/orderflow/orderflow_api/payload/CategoryDTO.java
@@ -0,0 +1,13 @@
+package com.orderflow.orderflow_api.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+    private Long id;
+    private String categoryName;
+}
