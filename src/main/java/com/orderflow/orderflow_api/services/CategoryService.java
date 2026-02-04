c6526bda33e222d406a7856e66ab980bdb8b69f1
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-02-04 00:34:10 -0300

    feat: category services and controllers

--- src/main/java/com/orderflow/orderflow_api/services/CategoryService.java
+++ src/main/java/com/orderflow/orderflow_api/services/CategoryService.java
@@ -1,4 +1,8 @@
 package com.orderflow.orderflow_api.services;
 
+import com.orderflow.orderflow_api.exceptions.APIException;
+import com.orderflow.orderflow_api.payload.CategoryResponse;
+
 public interface CategoryService {
+    CategoryResponse getAllCategories() throws APIException;
 }
