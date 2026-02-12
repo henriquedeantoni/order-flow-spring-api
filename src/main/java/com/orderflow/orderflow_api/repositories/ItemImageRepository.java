f6de347f6f56f32a4db80f26d4d7ac840e00f77f
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-02-11 21:40:55 -0300

    fix: item image repository long id

--- src/main/java/com/orderflow/orderflow_api/repositories/ItemImageRepository.java
+++ src/main/java/com/orderflow/orderflow_api/repositories/ItemImageRepository.java
@@ -3,5 +3,5 @@ package com.orderflow.orderflow_api.repositories;
 import com.orderflow.orderflow_api.models.ItemImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemImageRepository extends JpaRepository<ItemImage, String> {
+public interface ItemImageRepository extends JpaRepository<ItemImage, Long> {
 }
