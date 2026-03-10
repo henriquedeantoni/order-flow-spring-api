aafc8c6bb5f8468e066f10a27cae2b942c13c399
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-03-09 22:10:10 -0300

    feat: chart style and engine

---
+++ src/main/java/com/orderflow/orderflow_api/GraphicEngine/Styles/ChartTheme.java
@@ -0,0 +1,42 @@
+package com.orderflow.orderflow_api.GraphicEngine.Styles;
+
+import java.awt.*;
+
+public class ChartTheme {
+
+    private Color chartBackground;
+    private Color plotBackground;
+    private Color textColor;
+    private Color gridColor;
+
+    private Color primaryColor;
+    private Color secondaryColor;
+    private Color tertiaryColor;
+
+    public ChartTheme(
+            Color chartBackground,
+            Color plotBackground,
+            Color textColor,
+            Color gridColor,
+            Color primaryColor,
+            Color secondaryColor,
+            Color tertiaryColor
+    ) {
+        this.chartBackground = chartBackground;
+        this.plotBackground = plotBackground;
+        this.textColor = textColor;
+        this.gridColor = gridColor;
+        this.primaryColor = primaryColor;
+        this.secondaryColor = secondaryColor;
+        this.tertiaryColor = tertiaryColor;
+    }
+
+    public Color getChartBackground() { return chartBackground; }
+    public Color getPlotBackground() { return plotBackground; }
+    public Color getTextColor() { return textColor; }
+    public Color getGridColor() { return gridColor; }
+
+    public Color getPrimaryColor() { return primaryColor; }
+    public Color getSecondaryColor() { return secondaryColor; }
+    public Color getTertiaryColor() { return tertiaryColor; }
+}
--- src/main/java/com/orderflow/orderflow_api/services/DashboardService.java
+++ src/main/java/com/orderflow/orderflow_api/services/DashboardService.java
@@ -5,5 +5,5 @@ import java.io.IOException;
 public interface DashboardService {
     String getExampleChart() throws IOException;
 
-    String getDashboardExampleWithSize(int width, int height) throws IOException;
+    String getDashboardExampleWithSize(int width, int height, String theme) throws IOException;
 }
