1696b37ce140ee6498d47ed8b7799fc9a6d5907d
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-04-13 23:53:30 -0300

    feat: new method on supply registration

--- src/main/java/com/orderflow/orderflow_api/repositories/SupplyRepository.java
+++ src/main/java/com/orderflow/orderflow_api/repositories/SupplyRepository.java
@@ -2,10 +2,12 @@ package com.orderflow.orderflow_api.repositories;
 
 import com.orderflow.orderflow_api.models.Supply;
 import com.orderflow.orderflow_api.payload.SupplyDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SupplyRepository extends CrudRepository<Supply, Long> {
+public interface SupplyRepository extends JpaRepository<Supply, Long>, JpaSpecificationExecutor<Supply> {
     Supply findBySupplyReference(String supplyReference);
 }
--- src/main/java/com/orderflow/orderflow_api/services/SupplyServiceImpl.java
+++ src/main/java/com/orderflow/orderflow_api/services/SupplyServiceImpl.java
@@ -9,7 +9,15 @@ import com.orderflow.orderflow_api.repositories.InventorySupplyRepository;
 import com.orderflow.orderflow_api.repositories.SupplyRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SupplyServiceImpl implements SupplyService {
@@ -61,6 +69,33 @@ public class SupplyServiceImpl implements SupplyService {
 
     @Override
     public SupplyResponse getAllSupplyRegistered(String keyword, Integer pageSize, Integer pageNumber, String sortBy, String sortOrder) {
-        return null;
+        Sort sortByAndOrder = sortOrder.equalsIgnoreCase("asc")
+                ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize, sortByAndOrder);
+        Specification<Supply> specification = Specification.allOf(List.of());
+        if(keyword != null && !keyword.isEmpty()) {
+            specification = specification.and((root, query, criteriaBuilder) ->
+                    criteriaBuilder.like(criteriaBuilder.lower(root.get("supplyName")),
+                            "%" + keyword.toLowerCase() + "%"));
+        }
+
+        Page<Supply> pageSupplies = supplyRepository.findAll(specification, pageDetails);
+
+        List<Supply> supplies = pageSupplies.getContent();
+        List<SupplyDTO> suppliesDTOS = supplies
+                .stream()
+                .map(supply -> {
+                    return modelMapper.map(supply, SupplyDTO.class);
+                }).toList();
+
+        SupplyResponse supplyResponse = new SupplyResponse();
+        supplyResponse.setContent(suppliesDTOS);
+        supplyResponse.setPageNumber(pageSupplies.getNumber());
+        supplyResponse.setPageSize(pageSupplies.getSize());
+        supplyResponse.setTotalElements(pageSupplies.getTotalElements());
+        supplyResponse.setTotalPages(pageSupplies.getTotalPages());
+        return supplyResponse;
     }
 }
