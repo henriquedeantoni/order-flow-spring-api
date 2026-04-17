5cf1e1bc03a5a250c4d9640d9c4009eb53689b31
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-04-16 23:00:55 -0300

    feat: recipe and recipe supply models

--- src/main/java/com/orderflow/orderflow_api/models/Recipe.java
+++ src/main/java/com/orderflow/orderflow_api/models/Recipe.java
@@ -1,4 +1,36 @@
 package com.orderflow.orderflow_api.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "recipes")
 public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long recipeId;
+
+    private String recipeName;
+
+    private String preparationDescription;
+
+    @ManyToOne
+    public Item item;
+
+    @OneToMany(mappedBy = "recipe", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    public List<RecipeSupply> recipeSupplies = new ArrayList<>();
+
+    public OffsetDateTime includedDate = OffsetDateTime.now();
+    public OffsetDateTime updatedDate;
+
+    public int timeMinutesToPrepare;
 }
--- src/main/java/com/orderflow/orderflow_api/models/RecipeSupply.java
+++ src/main/java/com/orderflow/orderflow_api/models/RecipeSupply.java
@@ -2,14 +2,14 @@ package com.orderflow.orderflow_api.models;
 
 import jakarta.persistence.*;
 
-public class RecipeItem {
+public class RecipeSupply {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long recipeId;
 
     @ManyToOne
-    @JoinColumn(name = "item_id")
-    private Item item;
+    @JoinColumn(name = "supply_id")
+    private Supply supply;
 
     @ManyToOne
     @JoinColumn(name = "recipe_id")
