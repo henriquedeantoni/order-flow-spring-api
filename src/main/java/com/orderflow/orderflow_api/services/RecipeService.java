b1fd40c750d614e7931b76c18d0d9f406de03291
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-04-17 23:59:41 -0300

    feat: recipe controller

--- src/main/java/com/orderflow/orderflow_api/services/RecipeService.java
+++ src/main/java/com/orderflow/orderflow_api/services/RecipeService.java
@@ -1,4 +1,7 @@
 package com.orderflow.orderflow_api.services;
 
+import com.orderflow.orderflow_api.payload.RecipeDTO;
+
 public interface RecipeService {
+    RecipeDTO findRecipeById(Long recipeId);
 }
--- src/main/java/com/orderflow/orderflow_api/services/RecipeServiceImpl.java
+++ src/main/java/com/orderflow/orderflow_api/services/RecipeServiceImpl.java
@@ -1,7 +1,12 @@
 package com.orderflow.orderflow_api.services;
 
+import com.orderflow.orderflow_api.payload.RecipeDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RecipeServiceImpl implements RecipeService {
+    @Override
+    public RecipeDTO findRecipeById(Long recipeId) {
+        return null;
+    }
 }
