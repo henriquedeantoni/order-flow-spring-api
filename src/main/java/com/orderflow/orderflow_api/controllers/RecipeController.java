b8bc2c9bdd3a656d97dc761e984714c9418519a1
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-04-20 23:58:49 -0300

    feat: recipe supply list updated

--- src/main/java/com/orderflow/orderflow_api/controllers/RecipeController.java
+++ src/main/java/com/orderflow/orderflow_api/controllers/RecipeController.java
@@ -1,12 +1,15 @@
 package com.orderflow.orderflow_api.controllers;
 
 import com.orderflow.orderflow_api.payload.RecipeDTO;
+import com.orderflow.orderflow_api.payload.RecipeSupplyDTO;
 import com.orderflow.orderflow_api.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("v1/")
@@ -25,10 +28,12 @@ public class RecipeController {
 
     @PostMapping("/admin/recipes")
     public ResponseEntity<RecipeDTO> registerRecipe(
+            @RequestParam Long itemId,
             @RequestBody RecipeDTO recipeDTO,
-            @RequestParam Long itemId
+            @RequestBody List<RecipeSupplyDTO> recipeList
+
     ) {
-        RecipeDTO createdRecipe = recipeService.registerRecipe(recipeDTO, itemId);
+        RecipeDTO createdRecipe = recipeService.registerRecipe(recipeDTO, itemId, recipeList);
         return new ResponseEntity<>(createdRecipe, HttpStatus.CREATED);
     }
 
--- src/main/java/com/orderflow/orderflow_api/services/RecipeServiceImpl.java
+++ src/main/java/com/orderflow/orderflow_api/services/RecipeServiceImpl.java
@@ -3,20 +3,28 @@ package com.orderflow.orderflow_api.services;
 import com.orderflow.orderflow_api.exceptions.ResourceNotFoundException;
 import com.orderflow.orderflow_api.models.Item;
 import com.orderflow.orderflow_api.models.Recipe;
+import com.orderflow.orderflow_api.models.RecipeSupply;
 import com.orderflow.orderflow_api.payload.RecipeDTO;
+import com.orderflow.orderflow_api.payload.RecipeSupplyDTO;
 import com.orderflow.orderflow_api.repositories.ItemRepository;
 import com.orderflow.orderflow_api.repositories.RecipeRepository;
+import com.orderflow.orderflow_api.repositories.RecipeSupplyRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
     @Autowired
     private RecipeRepository recipeRepository;
+
+    @Autowired
+    private RecipeSupplyRepository recipeSupplyRepository;
 
     @Autowired
     private ModelMapper modelMapper;
@@ -34,9 +42,14 @@ public class RecipeServiceImpl implements RecipeService {
 
 
     @Override
-    public RecipeDTO registerRecipe(RecipeDTO recipeDTO, Long itemId) {
+    public RecipeDTO registerRecipe(RecipeDTO recipeDTO, Long itemId, List<RecipeSupplyDTO> recipeList) {
         Item itemFromDb = itemRepository.findById(itemId)
                 .orElseThrow(()-> new ResourceNotFoundException("Item", "itemId", itemId));
+
+        for(RecipeSupplyDTO recipeSupplyDTO : recipeList){
+            var recipeSupplyFromDb = recipeSupplyRepository.findById(recipeSupplyDTO.getRecipeSupplyId())
+                    .orElseThrow(()-> new ResourceNotFoundException("RecipeSupply", "recipeSupplyId", recipeSupplyDTO.getRecipeSupplyId()));
+        }
 
         Recipe recipe = new Recipe();
         recipe.setRecipeName(recipeDTO.getRecipeName());
@@ -46,6 +59,14 @@ public class RecipeServiceImpl implements RecipeService {
         recipe.setUpdatedDate(OffsetDateTime.now());
         recipe.setItem(itemFromDb);
 
+        List<RecipeSupply> recipeSupply = new ArrayList<>();
+        recipeSupply = recipeList.stream()
+                        .map(rec ->{
+                            return modelMapper.map(rec, RecipeSupply.class);
+                        }).toList();
+
+        recipe.setRecipeSupplies(recipeSupply);
+
         recipeRepository.save(recipe);
 
         return modelMapper.map(recipe, RecipeDTO.class);
@@ -53,6 +74,20 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public RecipeDTO updateRecipe(RecipeDTO recipeDTO, Long recipeId) {
+        Recipe recipeFromDb = recipeRepository.findById(recipeId)
+                .orElseThrow(()-> new ResourceNotFoundException("Recipe", "recipeId", recipeId));
+
+        recipeFromDb.setRecipeName(recipeDTO.getRecipeName());
+        recipeFromDb.setPreparationDescription(recipeDTO.getPreparationDescription());
+        recipeFromDb.setTimeMinutesToPrepare(recipeDTO.getTimeMinutesToPrepare());
+        recipeFromDb.setUpdatedDate(OffsetDateTime.now());
+        recipeRepository.save(recipeFromDb);
+
+        return modelMapper.map(recipeFromDb, RecipeDTO.class);
+    }
+
+    @Override
+    public RecipeDTO updateRecipeList(List<RecipeSupplyDTO> recipeList, Long recipeId) {
         Recipe recipeFromDb = recipeRepository.findById(recipeId)
                 .orElseThrow(()-> new ResourceNotFoundException("Recipe", "recipeId", recipeId));
 
