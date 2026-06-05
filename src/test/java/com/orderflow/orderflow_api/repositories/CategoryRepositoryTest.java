ac14c8e025afcf13150f7cd1ce703c38fe9ea101
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-06-04 23:55:12 -0300

    test: category find by id and find all testing cases

--- src/test/java/com/orderflow/orderflow_api/repositories/CategoryRepositoryTest.java
+++ src/test/java/com/orderflow/orderflow_api/repositories/CategoryRepositoryTest.java
@@ -34,16 +34,21 @@ public class CategoryRepositoryTest {
     private final Item itemFour = new Item();
     private final Item itemFive = new Item();
 
+    private final OffsetDateTime specificDateTime = OffsetDateTime.of(
+            2020, 1, 1, 12, 00, 0, 0, ZoneOffset.UTC
+    );
+
     @BeforeEach
     public void setUp(){
+
         firstCategory.setCategoryName("First Category");
-        firstCategory.setIncludedDate(OffsetDateTime.now(ZoneOffset.UTC));
+        firstCategory.setIncludedDate(specificDateTime);
 
         secondCategory.setCategoryName("Second Category");
-        secondCategory.setIncludedDate(OffsetDateTime.now(ZoneOffset.UTC));
+        secondCategory.setIncludedDate(specificDateTime);
 
         thirdCategory.setCategoryName("Third Category");
-        thirdCategory.setIncludedDate(OffsetDateTime.now(ZoneOffset.UTC));
+        thirdCategory.setIncludedDate(specificDateTime);
 
         itemOne.setItemName("item one");
         itemOne.setQuantity(10);
@@ -83,5 +88,41 @@ public class CategoryRepositoryTest {
         assertNotNull(savedCategory);
         assertTrue(savedCategory.getCategoryId()>0);
         assertEquals("First Category", savedCategory.getCategoryName());
+        assertEquals(specificDateTime, savedCategory.getIncludedDate());
+        assertEquals(firstCategory, savedCategory);
     }
+
+    @DisplayName("JUnit test Given Category List when Save then Return Category List")
+    @Test
+    public void testGivenCategoryList_whenSave_thenReturnCategoryList(){
+        // Given/Arrange
+        categoryRepository.save(firstCategory);
+        categoryRepository.save(secondCategory);
+        categoryRepository.save(thirdCategory);
+
+        // When/Act
+        List<Category> savedCategories = categoryRepository.findAll();
+
+        // Then/Assert
+        assertNotNull(savedCategories);
+        assertTrue(savedCategories.size()>0);
+        assertEquals(3, savedCategories.size());
+    }
+
+    @DisplayName("JUnit test for Category Object when find by id then return category object")
+    @Test
+    public void testGivenCategoryObject_whenFindById_thenReturnCategoryObject(){
+        // Given/Arrange
+        categoryRepository.save(firstCategory);
+
+        // When/Act
+        Category savedCategory = categoryRepository.save(firstCategory);
+
+        // Then/Assert
+        assertNotNull(savedCategory);
+        assertEquals(firstCategory.getCategoryId(), savedCategory.getCategoryId());
+    }
+
+
+
 }
