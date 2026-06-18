0c3df498243dbfd9caf626713af134407817c445
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-06-17 23:58:53 -0300

    test: recipe repository test

--- src/test/java/com/orderflow/orderflow_api/repositories/RecipeRepositoryTest.java
+++ src/test/java/com/orderflow/orderflow_api/repositories/RecipeRepositoryTest.java
@@ -1,129 +1,134 @@
 package com.orderflow.orderflow_api.repositories;
 
-import com.orderflow.orderflow_api.models.AlbumImage;
-import com.orderflow.orderflow_api.models.SimpleImage;
+import com.orderflow.orderflow_api.models.Recipe;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-public class SimpleImageRepositoryTest {
+public class RecipeRepositoryTest {
 
     @Autowired
-    private SimpleImageRepository simpleImageRepository;
+    private RecipeRepository recipeRepository;
 
-    @Autowired
-    private AlbumImageRepository albumImageRepository;
+    private Recipe recipeOne;
+    private Recipe recipeTwo;
+    private Recipe recipeThree;
+    private Recipe recipeFour;
 
-    private SimpleImage simpleImageOne;
-    private SimpleImage simpleImageTwo;
-    private SimpleImage simpleImageThree;
-    private SimpleImage simpleImageFour;
-
-    private AlbumImage albumImageOne;
+    private final OffsetDateTime includedDate = OffsetDateTime.of(
+            2020, 1, 1, 12, 00, 0, 0, ZoneOffset.UTC
+    );
 
     @BeforeEach
     public void setup() {
 
-        albumImageOne = new AlbumImage(
-                "Album Image One Title",
-                "Album Image One Description"
+        recipeOne = new Recipe(
+                "Recipe One",
+                "Recipe One Preparation Description",
+                includedDate,
+                25
+        );
+        recipeTwo = new Recipe(
+                "Recipe Two",
+                "Recipe Two Preparation Description",
+                includedDate,
+                50
+        );
+        recipeThree = new Recipe(
+                "Recipe Three",
+                "Recipe Three Preparation Description",
+                includedDate,
+                75
+        );
+        recipeFour = new Recipe(
+                "Recipe Four",
+                "Recipe Four Preparation Description",
+                includedDate,
+                50
         );
 
-        albumImageRepository.saveAndFlush(albumImageOne);
-
-        simpleImageOne = new SimpleImage(
-                "/imageOne/uuidCode",
-                "Simple Image One"
-        );
-        simpleImageTwo = new SimpleImage(
-                "/imageTwo/uuidCode",
-                "Simple Image Two"
-        );
-        simpleImageThree = new SimpleImage(
-                "/imageThree/uuidCode",
-                "Simple Image Three"
-        );
-        simpleImageFour = new SimpleImage(
-                "/imageFour/uuidCode",
-                "Simple Image Four"
-        );
-
+/*
         simpleImageOne.setAlbumImage(albumImageOne);
         simpleImageTwo.setAlbumImage(albumImageOne);
         simpleImageThree.setAlbumImage(albumImageOne);
         simpleImageFour.setAlbumImage(albumImageOne);
+        */
     }
 
-    @DisplayName("JUnit test for Given SimpleImage Object when Save then Return SimpleImage Object")
+    @DisplayName("JUnit test for Given Recipe Object when Save then Return Recipe Object")
     @Test
-    void testGivenSimpleImageObject_whenSave_thenReturnSimpleImageObject(){
+    void testGivenRecipeObject_whenSave_thenReturnRecipeObject(){
         // Given/Arrange
 
         // When/Act
-        SimpleImage savedSimpleImage = simpleImageRepository.save(simpleImageOne);
+        Recipe savedRecipe = recipeRepository.save(recipeOne);
 
         // Then/Assert
-        assertNotNull(savedSimpleImage);
-        assertTrue(savedSimpleImage.getSimpleImageId()>0);
-        assertEquals("/imageOne/uuidCode", savedSimpleImage.getUrl());
-        assertEquals("Simple Image One", savedSimpleImage.getTitle());
+        assertNotNull(savedRecipe);
+        assertTrue(savedRecipe.getRecipeId()>0);
+        assertEquals("Recipe One", savedRecipe.getRecipeName());
+        assertEquals("Recipe One Preparation Description", savedRecipe.getPreparationDescription());
+        assertEquals(includedDate, savedRecipe.getIncludedDate());
+        assertEquals(25, savedRecipe.getTimeMinutesToPrepare());
     }
 
-    @DisplayName("JUnit test for Given SimpleImage Object when Find By Id then Return Simple Image Object")
+    @DisplayName("JUnit test for Given Recipe Object when FindById then Return Recipe Object")
     @Test
-    void testGivenSimpleImageObject_whenFindById_thenReturnSimpleImageObject(){
+    void testGivenRecipeObject_whenFindById_thenReturnRecipeObject(){
         // Given/Arrange
-        simpleImageRepository.save(simpleImageOne);
-        simpleImageRepository.save(simpleImageTwo);
+        recipeRepository.save(recipeOne);
+        recipeRepository.save(recipeTwo);
 
         // When/Act
-        SimpleImage firstSimpleImage = simpleImageRepository.findById(simpleImageOne.getSimpleImageId()).get();
-        SimpleImage secondSimpleImage = simpleImageRepository.findById(simpleImageTwo.getSimpleImageId()).get();
+        Recipe firstRecipe = recipeRepository.findById(recipeOne.getRecipeId()).get();
+        Recipe secondRecipe = recipeRepository.findById(recipeTwo.getRecipeId()).get();
 
         // Then/Assert
-        assertNotNull(firstSimpleImage);
-        assertNotNull(secondSimpleImage);
-        assertEquals(simpleImageOne.getSimpleImageId(), firstSimpleImage.getSimpleImageId());
-        assertEquals(simpleImageTwo.getSimpleImageId(), secondSimpleImage.getSimpleImageId());
+        assertNotNull(firstRecipe);
+        assertNotNull(secondRecipe);
+        assertEquals(recipeOne.getRecipeId(), firstRecipe.getRecipeId());
+        assertEquals(recipeTwo.getRecipeId(), secondRecipe.getRecipeId());
     }
 
-    @DisplayName("JUnit test for Given SimpleImage Object when Update SimpleImage then Return SimpleImage Object")
+    @DisplayName("JUnit test for Given Recipe Object when Update Recipe then Return Recipe Object")
     @Test
-    void testGivenSimpleImageObject_whenUpdateSimpleImage_thenReturnUpdateObject(){
+    void testGivenSimpleRecipeObject_whenUpdateRecipe_thenReturnRecipeObject(){
         // Given/Arrange
-        simpleImageRepository.save(simpleImageOne);
+        recipeRepository.save(recipeOne);
 
         // When/Act
-        SimpleImage savedSimpleImage = simpleImageRepository.findById(simpleImageOne.getSimpleImageId()).get();
-        savedSimpleImage.setTitle("Changed Title");
-        savedSimpleImage.setUrl("Changed Url");
+        Recipe savedRecipe = recipeRepository.findById(recipeOne.getRecipeId()).get();
+        savedRecipe.setRecipeName("Changed Name");
+        savedRecipe.setPreparationDescription("Changed Description");
 
-        SimpleImage updatedSimpleImage = simpleImageRepository.save(savedSimpleImage);
+        Recipe updatedRecipe = recipeRepository.save(savedRecipe);
 
         // Then/Assert
-        assertNotNull(updatedSimpleImage);
-        assertEquals("Changed Title", updatedSimpleImage.getTitle());
-        assertEquals("Changed Url", updatedSimpleImage.getUrl());
+        assertNotNull(updatedRecipe);
+        assertEquals("Changed Name", updatedRecipe.getRecipeName());
+        assertEquals("Changed Description", updatedRecipe.getPreparationDescription());
     }
 
-    @DisplayName("JUnit test for Given SimpleImage Object when Delete SimpleImageById then Remove SimpleImage Object")
+    @DisplayName("JUnit test for Given Recipe Object when Delete Recipe then Remove Recipe Object")
     @Test
-    void testGivenSimpleImageObject_whenDeleteSimpleImageById_thenRemoveSimpleImage(){
+    void testGivenRecipeObject_whenDeleteRecipeById_thenRemoveRecipe(){
         // Given/Arrange
-        simpleImageRepository.save(simpleImageOne);
+        recipeRepository.save(recipeOne);
 
         // When/Act
-        simpleImageRepository.deleteById(simpleImageOne.getSimpleImageId());
-        Optional<SimpleImage> simpleImageOptional = simpleImageRepository.findById(simpleImageOne.getSimpleImageId());
+        recipeRepository.deleteById(recipeOne.getRecipeId());
+        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeOne.getRecipeId());
 
         // Then/Assert
-        assertTrue(simpleImageOptional.isEmpty());
+        assertTrue(recipeOptional.isEmpty());
     }
 }
