93a541bb0cd7af9e2fd274d3e0da3882eef1d87a
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-06-21 13:59:35 -0300

    test: included othe jpql queries methods tests

--- src/test/java/com/orderflow/orderflow_api/repositories/CartRepositoryTest.java
+++ src/test/java/com/orderflow/orderflow_api/repositories/CartRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.orderflow.orderflow_api.repositories;
 
 import com.orderflow.orderflow_api.models.Cart;
+import com.orderflow.orderflow_api.models.CartItem;
+import com.orderflow.orderflow_api.models.Item;
 import com.orderflow.orderflow_api.models.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +30,9 @@ public class CartRepositoryTest {
     @Autowired
     private CartItemRepository cartItemRepository;
 
+    @Autowired
+    private ItemRepository itemRepository;
+
     private final OffsetDateTime createDate = OffsetDateTime.of(
             2020, 1, 1, 12, 00, 0, 0, ZoneOffset.UTC
     );
@@ -34,6 +40,18 @@ public class CartRepositoryTest {
     private Cart cartOne;
     private Cart cartTwo;
     private Cart cartThree;
+
+    private Item itemOne;
+    private Item itemTwo;
+    private Item itemThree;
+
+    private CartItem cartItemOne;
+    private CartItem cartItemTwo;
+    private CartItem cartItemThree;
+
+    private CartItem cartItemFour;
+    private CartItem cartItemFive;
+    private CartItem cartItemSix;
 
     private User userOne;
     private User userTwo;
@@ -69,6 +87,50 @@ public class CartRepositoryTest {
         cartOne = new Cart(createDate, userOne, 25.00);
         cartTwo = new Cart(createDate, userTwo, 15.00);
         cartThree = new Cart(createDate, userThree, 50.00);
+
+        cartItemOne = new CartItem(2, 20.0, 125.00);
+        cartItemTwo = new CartItem(3, 5.0, 50.00);
+        cartItemThree = new CartItem(1, 0.0, 80.00);
+
+        cartItemFour = new CartItem(2, 20.0, 125.00);
+        cartItemFive = new CartItem(3, 5.0, 50.00);
+        cartItemSix = new CartItem(1, 0.0, 80.00);
+
+        itemOne = new Item("Item One","Item one description" ,1 ,125.00);
+        itemTwo = new Item("Item Two","Item two description" ,1 ,50.00);
+        itemThree = new Item("Item Three","Item three description" ,1 ,80.00);
+
+        itemOne.getItems().add(cartItemOne);
+        itemTwo.getItems().add(cartItemTwo);
+        itemThree.getItems().add(cartItemThree);
+
+        itemOne.getItems().add(cartItemFour);
+        itemTwo.getItems().add(cartItemFive);
+        itemThree.getItems().add(cartItemSix);
+
+        cartItemOne.setItem(itemOne);
+        cartItemTwo.setItem(itemTwo);
+        cartItemThree.setItem(itemThree);
+        cartItemFour.setItem(itemOne);
+        cartItemFive.setItem(itemTwo);
+        cartItemSix.setItem(itemThree);
+
+        //cartOne.setCartItems(List.of(cartItemOne, cartItemTwo, cartItemThree));
+        cartOne.setCartItems(new ArrayList<>(List.of(
+                cartItemOne,
+                cartItemTwo,
+                cartItemThree
+        )));
+
+        cartTwo.setCartItems(new ArrayList<>(List.of(
+                cartItemFour,
+                cartItemFive,
+                cartItemSix
+        )));
+
+        itemRepository.save(itemOne);
+        itemRepository.save(itemTwo);
+        itemRepository.save(itemThree);
 
         userRepository.save(userOne);
         userRepository.save(userTwo);
@@ -172,4 +234,55 @@ public class CartRepositoryTest {
         assertTrue(savedCart.getCartId()>0);
         assertEquals(userEmail, savedCart.getUser().getEmail());
     }
+
+    @DisplayName("JUnit test for Given CartItems and Items in Cart Object when find Cart By Item Id then Return Cart Object  ")
+    @Test
+    void testGivenCartItemsAndItemsInCartObject_whenFindCartByItemId_thenReturnCartObject(){
+        // Given/Arrange
+
+        cartItemOne.setCart(cartOne);
+        cartItemTwo.setCart(cartOne);
+        cartItemThree.setCart(cartOne);
+        cartItemRepository.save(cartItemOne);
+        cartItemRepository.save(cartItemTwo);
+        cartItemRepository.save(cartItemThree);
+
+        cartItemFour.setCart(cartTwo);
+        cartItemFive.setCart(cartTwo);
+        cartItemSix.setCart(cartTwo);
+        cartItemRepository.save(cartItemFour);
+        cartItemRepository.save(cartItemFive);
+        cartItemRepository.save(cartItemSix);
+
+        cartRepository.save(cartOne);
+        cartRepository.save(cartTwo);
+
+        Long itemId = cartItemOne.getItem().getItemId();
+
+        // When/Act
+        List<Cart> savedCarts = cartRepository.findCartsByItemId(itemId);
+
+        // Then/Assert
+        assertNotNull(savedCarts);
+        assertTrue(savedCarts.size()>0);
+        assertEquals(2, savedCarts.size());
+    }
+
+    @DisplayName("JUnit test for Given User email and Cart Object when find Cart By Email then Return Cart Object  ")
+    @Test
+    void testGivenUserEmailAndCart_whenFindCartByEmailAndCartId_thenReturnCartObject(){
+        // Given/Arrange
+        cartRepository.save(cartOne);
+        String userEmail = "userone@mail.com";
+        Long cartId = 1L;
+
+        // When/Act
+        Cart savedCart = cartRepository.findCartByEmail(userEmail);
+
+        // Then/Assert
+        assertNotNull(savedCart);
+        assertTrue(savedCart.getCartId()>0);
+        assertEquals(userEmail, savedCart.getUser().getEmail());
+    }
+
 }
