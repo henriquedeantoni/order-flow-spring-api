703b8e9db37547e376590201a2bdbfb6c05e29bb
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-06-28 14:30:29 -0300

    test: FindBySupplyId and FindByEventDateGreaterThanEqualAndEventDateLessThanEqual methods test

--- src/test/java/com/orderflow/orderflow_api/repositories/SupplyEventRepositoryTest.java
+++ src/test/java/com/orderflow/orderflow_api/repositories/SupplyEventRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -38,6 +39,10 @@ public class SupplyEventRepositoryTest {
     private SupplyEvent supplyEventTwo;
     private SupplyEvent supplyEventThree;
     private SupplyEvent supplyEventFour;
+    private SupplyEvent supplyEventFive;
+    private SupplyEvent supplyEventSix;
+    private SupplyEvent supplyEventSeven;
+    private SupplyEvent supplyEventEight;
 
     @BeforeEach
     public void setup() {
@@ -64,6 +69,34 @@ public class SupplyEventRepositoryTest {
 
         supplyEventFour = new SupplyEvent(
                 1L,
+                "STOCK_IN",
+                35,
+                fourthDate
+        );
+
+        supplyEventFive = new SupplyEvent(
+                2L,
+                "STOCK_IN",
+                50,
+                firstDate
+        );
+
+        supplyEventSix = new SupplyEvent(
+                2L,
+                "STOCK_OUT",
+                25,
+                secondDate
+        );
+
+        supplyEventSeven = new SupplyEvent(
+                2L,
+                "STOCK_OUT",
+                10,
+                thirdDate
+        );
+
+        supplyEventEight = new SupplyEvent(
+                2L,
                 "STOCK_IN",
                 35,
                 fourthDate
@@ -136,6 +169,51 @@ public class SupplyEventRepositoryTest {
 
         // Then/Assert
         assertTrue(supplyOptional.isEmpty());
+    }
+
+    @DisplayName("JUnit test for Given Supply Event Objects when Find By Supply Id then Return Supply Event Object List")
+    @Test
+    void testGivenSupplyEventObjects_whenFindBySupplyId_thenReturnSupplyEventObjects(){
+        // Given/Arrange
+        supplyEventRepository.save(supplyEventOne);
+        supplyEventRepository.save(supplyEventTwo);
+        supplyEventRepository.save(supplyEventThree);
+        supplyEventRepository.save(supplyEventFour);
+        supplyEventRepository.save(supplyEventFive);
+        supplyEventRepository.save(supplyEventSix);
+        supplyEventRepository.save(supplyEventSeven);
+        supplyEventRepository.save(supplyEventEight);
+
+        // When/Act
+        List<SupplyEvent> supplyEventsListOne = supplyEventRepository.findAllBySupplyId(1L);
+        List<SupplyEvent> supplyEventsListTwo = supplyEventRepository.findAllBySupplyId(2L);
+
+        // Then/Assert
+        assertTrue(supplyEventsListOne.size()>0);
+        assertEquals(4, supplyEventsListOne.size());
+        assertTrue(supplyEventsListTwo.size()>0);
+        assertEquals(4, supplyEventsListTwo.size());
+    }
+
+    @DisplayName("JUnit test for Given Supply Event Objects when Find By EventDate Greater Than Equal And EventDate Less Than Equal then Return Supply Event Object List")
+    @Test
+    void testGivenSupplyEventObjects_whenFindByEventDateGreaterThanEqualAndEventDateLessThanEqual_thenReturnSupplyEventObjects(){
+        // Given/Arrange
+        supplyEventRepository.save(supplyEventOne);
+        supplyEventRepository.save(supplyEventTwo);
+        supplyEventRepository.save(supplyEventThree);
+        supplyEventRepository.save(supplyEventFour);
+        supplyEventRepository.save(supplyEventFive);
+        supplyEventRepository.save(supplyEventSix);
+        supplyEventRepository.save(supplyEventSeven);
+        supplyEventRepository.save(supplyEventEight);
+
+        // When/Act
+        List<SupplyEvent> supplyEventsListOne = supplyEventRepository.findByEventDateGreaterThanEqualAndEventDateLessThanEqual(secondDate, fourthDate);
+
+        // Then/Assert
+        assertTrue(supplyEventsListOne.size()>0);
+        assertEquals(6, supplyEventsListOne.size());
     }
 
 }
