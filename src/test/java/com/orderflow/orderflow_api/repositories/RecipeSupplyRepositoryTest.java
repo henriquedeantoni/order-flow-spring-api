64e63e4b5df2eb904096e653e2d2c00fe4e07e5a
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-07-04 13:22:30 -0300

    test: completed all methods on recipe suppy repository

--- src/test/java/com/orderflow/orderflow_api/repositories/RecipeSupplyRepositoryTest.java
+++ src/test/java/com/orderflow/orderflow_api/repositories/RecipeSupplyRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.orderflow.orderflow_api.repositories;
 
 import com.orderflow.orderflow_api.models.Recipe;
+import com.orderflow.orderflow_api.models.RecipeSupply;
+import com.orderflow.orderflow_api.models.Supply;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,15 +16,25 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-public class RecipeRepositoryTest {
+public class RecipeSupplyRepositoryTest {
+
+    @Autowired
+    private RecipeSupplyRepository recipeSupplyRepository;
 
     @Autowired
     private RecipeRepository recipeRepository;
 
+    @Autowired
+    private SupplyRepository supplyRepository;
+
     private Recipe recipeOne;
     private Recipe recipeTwo;
-    private Recipe recipeThree;
-    private Recipe recipeFour;
+
+    private Supply supplyOne;
+    private Supply supplyTwo;
+
+    private RecipeSupply recipeSupplyOne;
+    private RecipeSupply recipeSupplyTwo;
 
     private final OffsetDateTime includedDate = OffsetDateTime.of(
             2020, 1, 1, 12, 00, 0, 0, ZoneOffset.UTC
@@ -43,92 +55,118 @@ public class RecipeRepositoryTest {
                 includedDate,
                 50
         );
-        recipeThree = new Recipe(
-                "Recipe Three",
-                "Recipe Three Preparation Description",
-                includedDate,
-                75
-        );
-        recipeFour = new Recipe(
-                "Recipe Four",
-                "Recipe Four Preparation Description",
-                includedDate,
-                50
+
+        supplyOne = new Supply(
+                "Supply One",
+                "Supply One Reference",
+                "Supply One Brand Name",
+                "Supply One Description",
+                "Supply One Code",
+                "Supply One Unit"
         );
 
-/*
-        simpleImageOne.setAlbumImage(albumImageOne);
-        simpleImageTwo.setAlbumImage(albumImageOne);
-        simpleImageThree.setAlbumImage(albumImageOne);
-        simpleImageFour.setAlbumImage(albumImageOne);
-        */
-    }
+        supplyTwo = new Supply(
+                "Supply Two",
+                "Supply Two Reference",
+                "Supply Two Brand Name",
+                "Supply Two Description",
+                "Supply Two Code",
+                "Supply Two Unit"
+        );
 
-    @DisplayName("JUnit test for Given Recipe Object when Save then Return Recipe Object")
-    @Test
-    void testGivenRecipeObject_whenSave_thenReturnRecipeObject(){
-        // Given/Arrange
-
-        // When/Act
-        Recipe savedRecipe = recipeRepository.save(recipeOne);
-
-        // Then/Assert
-        assertNotNull(savedRecipe);
-        assertTrue(savedRecipe.getRecipeId()>0);
-        assertEquals("Recipe One", savedRecipe.getRecipeName());
-        assertEquals("Recipe One Preparation Description", savedRecipe.getPreparationDescription());
-        assertEquals(includedDate, savedRecipe.getIncludedDate());
-        assertEquals(25, savedRecipe.getTimeMinutesToPrepare());
-    }
-
-    @DisplayName("JUnit test for Given Recipe Object when FindById then Return Recipe Object")
-    @Test
-    void testGivenRecipeObject_whenFindById_thenReturnRecipeObject(){
-        // Given/Arrange
         recipeRepository.save(recipeOne);
         recipeRepository.save(recipeTwo);
 
-        // When/Act
-        Recipe firstRecipe = recipeRepository.findById(recipeOne.getRecipeId()).get();
-        Recipe secondRecipe = recipeRepository.findById(recipeTwo.getRecipeId()).get();
+        supplyRepository.save(supplyOne);
+        supplyRepository.save(supplyTwo);
 
-        // Then/Assert
-        assertNotNull(firstRecipe);
-        assertNotNull(secondRecipe);
-        assertEquals(recipeOne.getRecipeId(), firstRecipe.getRecipeId());
-        assertEquals(recipeTwo.getRecipeId(), secondRecipe.getRecipeId());
+        recipeSupplyOne = new RecipeSupply(
+                supplyOne,
+                recipeOne,
+                20,
+                "gr"
+        );
+
+        recipeSupplyTwo = new RecipeSupply(
+                supplyTwo,
+                recipeTwo,
+                25,
+                "gr"
+        );
     }
 
-    @DisplayName("JUnit test for Given Recipe Object when Update Recipe then Return Recipe Object")
+    @DisplayName("JUnit test for Given Recipe Supply Object when Save Object then Return Recipe Supply Object")
     @Test
-    void testGivenSimpleRecipeObject_whenUpdateRecipe_thenReturnRecipeObject(){
+    void testGivenRecipeSupplyObject_whenSaveObject_thenReturnRecipeSupplyObject(){
         // Given/Arrange
-        recipeRepository.save(recipeOne);
 
         // When/Act
-        Recipe savedRecipe = recipeRepository.findById(recipeOne.getRecipeId()).get();
-        savedRecipe.setRecipeName("Changed Name");
-        savedRecipe.setPreparationDescription("Changed Description");
-
-        Recipe updatedRecipe = recipeRepository.save(savedRecipe);
+        RecipeSupply savedRecipeSupply = recipeSupplyRepository.save(recipeSupplyOne);
 
         // Then/Assert
-        assertNotNull(updatedRecipe);
-        assertEquals("Changed Name", updatedRecipe.getRecipeName());
-        assertEquals("Changed Description", updatedRecipe.getPreparationDescription());
+        assertNotNull(savedRecipeSupply);
+        assertTrue(savedRecipeSupply.getRecipeSupplyId()>0);
+        assertEquals(supplyOne, savedRecipeSupply.getSupply());
+        assertEquals(recipeOne, savedRecipeSupply.getRecipe());
+        assertEquals(20, savedRecipeSupply.getQuantity());
+        assertEquals("gr",  savedRecipeSupply.getUnit());
     }
 
-    @DisplayName("JUnit test for Given Recipe Object when Delete Recipe then Remove Recipe Object")
+    @DisplayName("JUnit test for Given Recipe Supply Object when Save Object then Return Recipe Supply Object")
     @Test
-    void testGivenRecipeObject_whenDeleteRecipeById_thenRemoveRecipe(){
+    void testGivenRecipeSupplyObject_whenFindById_thenReturnRecipeSupplyObject(){
         // Given/Arrange
-        recipeRepository.save(recipeOne);
-
+        recipeSupplyRepository.save(recipeSupplyOne);
         // When/Act
-        recipeRepository.deleteById(recipeOne.getRecipeId());
-        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeOne.getRecipeId());
+        RecipeSupply savedRecipeSupply = recipeSupplyRepository.findById(recipeSupplyOne.getRecipeSupplyId()).get();
 
         // Then/Assert
-        assertTrue(recipeOptional.isEmpty());
+        assertNotNull(savedRecipeSupply);
+        assertTrue(savedRecipeSupply.getRecipeSupplyId()>0);
+        assertEquals(recipeSupplyOne.getRecipeSupplyId(), savedRecipeSupply.getRecipeSupplyId());
+        assertEquals(supplyOne, savedRecipeSupply.getSupply());
+        assertEquals(recipeOne, savedRecipeSupply.getRecipe());
+        assertEquals(20, savedRecipeSupply.getQuantity());
+        assertEquals("gr",  savedRecipeSupply.getUnit());
     }
+
+    @DisplayName("JUnit test for Given Recipe Supply Object when Save Object then Return Recipe Supply Object")
+    @Test
+    void testGivenRecipeSupplyObject_whenUpdateRecipeSupply_thenReturnRecipeSupplyUpdated(){
+        // Given/Arrange
+        recipeSupplyRepository.save(recipeSupplyOne);
+        // When/Act
+        RecipeSupply savedRecipeSupply = recipeSupplyRepository.findById(recipeSupplyOne.getRecipeSupplyId()).get();
+
+        savedRecipeSupply.setSupply(supplyTwo);
+        savedRecipeSupply.setRecipe(recipeTwo);
+        savedRecipeSupply.setQuantity(1000);
+        recipeSupplyRepository.save(savedRecipeSupply);
+
+        // Then/Assert
+        assertNotNull(savedRecipeSupply);
+        assertTrue(savedRecipeSupply.getRecipeSupplyId()>0);
+        assertEquals(recipeSupplyOne.getRecipeSupplyId(), savedRecipeSupply.getRecipeSupplyId());
+        assertEquals(supplyTwo, savedRecipeSupply.getSupply());
+        assertEquals(recipeTwo, savedRecipeSupply.getRecipe());
+        assertEquals(1000, savedRecipeSupply.getQuantity());
+        assertEquals("gr",  savedRecipeSupply.getUnit());
+    }
+
+    @DisplayName("JUnit test for Given Recipe Supply Object when Save Object then Return Recipe Supply Object")
+    @Test
+    void testGivenRecipeSupplyObject_whenDeleteRecipeSupplyById_thenReturnRecipeSupplyUpdated(){
+        // Given/Arrange
+        recipeSupplyRepository.save(recipeSupplyOne);
+
+        // When/Act
+       recipeSupplyRepository.deleteById(recipeSupplyOne.getRecipeSupplyId());
+
+       Optional<RecipeSupply> recipeSupplyOptional = recipeSupplyRepository.findById(recipeSupplyOne.getRecipeSupplyId());
+
+        // Then/Assert
+        assertTrue(recipeSupplyOptional.isEmpty());
+    }
+
+
 }
