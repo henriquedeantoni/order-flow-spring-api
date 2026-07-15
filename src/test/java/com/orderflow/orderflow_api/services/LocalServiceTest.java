ba0d2b36db8c615a00a335b3669146f713d237ca
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-07-14 23:51:53 -0300

    test: local service findall and findallbyuser methods junit test

--- src/test/java/com/orderflow/orderflow_api/services/LocalServiceTest.java
+++ src/test/java/com/orderflow/orderflow_api/services/LocalServiceTest.java
@@ -5,6 +5,7 @@ import com.orderflow.orderflow_api.models.Local;
 import com.orderflow.orderflow_api.models.User;
 import com.orderflow.orderflow_api.payload.CategoryDTO;
 import com.orderflow.orderflow_api.payload.LocalDTO;
+import com.orderflow.orderflow_api.payload.LocalResponse;
 import com.orderflow.orderflow_api.repositories.LocalRepository;
 import com.orderflow.orderflow_api.security.util.AuthUtil;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,13 +16,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -29,7 +34,6 @@ import static org.mockito.BDDMockito.given;
 @ExtendWith(SpringExtension.class)
 public class LocalServiceTest {
 
-    @InjectMocks
     private ModelMapper modelMapper;
 
     @Mock
@@ -48,7 +52,8 @@ public class LocalServiceTest {
 
     @BeforeEach
     public void setup() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
+        modelMapper = new ModelMapper();
         ReflectionTestUtils.setField(localService, "modelMapper", new ModelMapper());
 
         localOne = new Local("Street A", "Building A", "City A", "State A", "Country A", "PostalCode A");
@@ -74,5 +79,53 @@ public class LocalServiceTest {
         assertEquals("Street A", savedLocal.getStreetName());
     }
 
+
+    @DisplayName("JUnit test for Given Local Object List When Find All Locals Then Return Local Response Object")
+    @Test
+    void testGivenLocalObjectListWhenFindAllLocalsThenReturnLocalResponseObject() {
+        // Given/Arrange
+        given(authUtil.userOnLoggedSession()).willReturn(new User("username", "user@email.com", "hashPass", "firstName", "lastName"));
+
+        List<Local> localMockList = List.of(
+                localOne,
+                localTwo,
+                localThree,
+                localFour);
+
+        Page<Local> localMockPage = new PageImpl(localMockList, PageRequest.of(0, 10), localMockList.size());
+
+        given(localRepository.findAll(any(PageRequest.class))).willReturn(localMockPage);
+
+        // When/Act
+        LocalResponse localResponse = localService.findAllLocals(0, 1, "streetName", "asc");
+
+        // Then/Assert
+        assertNotNull(localResponse);
+        assertEquals(4L, localResponse.getTotalElements());
+        assertEquals(1, localResponse.getTotalPages());
+        assertEquals(0, localResponse.getPageNumber());
+        assertTrue(localResponse.isLastPage());
+    }
+
+    @DisplayName("JUnit test for Given Local Object List When Find All User Locals Then Return Local Response Object ")
+    @Test
+    void testGivenLocalObjectListWhenFindAllUserLocalsThenReturnLocalResponseObject() {
+        // Given/Arrange
+        given(authUtil.userOnLoggedSession())
+                .willReturn(new User("username", "user@email.com", "hashPass", "firstName", "lastName"));
+        List<Local> localMockList = List.of(localOne, localTwo, localThree, localFour);
+        Page<Local> localMockPage = new PageImpl(localMockList, PageRequest.of(0, 10), localMockList.size());
+        given(localRepository.findAll(any(PageRequest.class))).willReturn(localMockPage);
+
+        // When/Act
+        LocalResponse localResponse = localService.findAllUserLocals(0, 1, "streetName", "asc");
+
+        // Then/Assert
+        assertNotNull(localResponse);
+        assertEquals(4L, localResponse.getTotalElements());
+        assertEquals(1, localResponse.getTotalPages());
+        assertEquals(0, localResponse.getPageNumber());
+        assertTrue(localResponse.isLastPage());
+    }
 
 }
