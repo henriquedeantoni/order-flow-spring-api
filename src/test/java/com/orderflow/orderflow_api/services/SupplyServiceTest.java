9272e2475af8a46f94c0d8e44da60384fc169f96
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-08-03 23:38:20 -0300

    test: updated supply service test methods

--- src/test/java/com/orderflow/orderflow_api/services/SupplyServiceTest.java
+++ src/test/java/com/orderflow/orderflow_api/services/SupplyServiceTest.java
@@ -1,7 +1,7 @@
 package com.orderflow.orderflow_api.services;
 
 import com.orderflow.orderflow_api.exceptions.APIException;
-import com.orderflow.orderflow_api.models.InventorySupply;
+import com.orderflow.orderflow_api.exceptions.ResourceNotFoundException;
 import com.orderflow.orderflow_api.models.Supply;
 import com.orderflow.orderflow_api.payload.SupplyDTO;
 import com.orderflow.orderflow_api.repositories.InventorySupplyRepository;
@@ -14,11 +14,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -46,7 +51,11 @@ public class SupplyServiceTest {
     private Supply supplyThree = new Supply();
     private Supply supplyFour = new Supply();
 
-    private OffsetDateTime dateTime = OffsetDateTime.of(2025, 10, 10, 10, 10, 10, 0, ZoneOffset.UTC);
+    private OffsetDateTime dateTimeOne = OffsetDateTime.of(2025, 10, 5, 10, 10, 10, 0, ZoneOffset.UTC);
+    private OffsetDateTime dateTimeTwo = OffsetDateTime.of(2025, 10, 10, 10, 10, 10, 0, ZoneOffset.UTC);
+    private OffsetDateTime dateTimeThree = OffsetDateTime.of(2025, 10, 15, 10, 10, 10, 0, ZoneOffset.UTC);
+    private OffsetDateTime dateTimeFour = OffsetDateTime.of(2025, 10, 20, 10, 10, 10, 0, ZoneOffset.UTC);
+
 
     @BeforeEach
     public void setUp()
@@ -60,19 +69,37 @@ public class SupplyServiceTest {
         supplyOne.setBrandName("BrandNameOne");
         supplyOne.setSupplyCode("SupplyCodeOne");
         supplyOne.setSupplyUnit("SupplyUnitOne");
-        supplyOne.setAddDate(dateTime);
+        supplyOne.setAddDate(dateTimeOne);
 
         supplyTwo.setSupplyReference("SupplyReferenceTwo");
         supplyTwo.setSupplyName("SupplyNameTwo");
         supplyTwo.setSupplyDescription("SupplyDescriptionTwo");
         supplyTwo.setSupplyReference("SupplyReferenceTwo");
         supplyTwo.setBrandName("BrandNameTwo");
+        supplyOne.setSupplyCode("SupplyCodeTwo");
+        supplyOne.setSupplyUnit("SupplyUnitTwo");
+        supplyOne.setAddDate(dateTimeTwo);
+
 
         supplyThree.setSupplyReference("SupplyReferenceThree");
         supplyThree.setSupplyName("SupplyNameThree");
         supplyThree.setSupplyDescription("SupplyDescriptionThree");
         supplyThree.setSupplyReference("SupplyReferenceThree");
         supplyThree.setBrandName("BrandNameThree");
+        supplyOne.setSupplyCode("SupplyCodeThree");
+        supplyOne.setSupplyUnit("SupplyUnitThree");
+        supplyOne.setAddDate(dateTimeThree);
+
+
+        supplyThree.setSupplyReference("SupplyReferenceFour");
+        supplyThree.setSupplyName("SupplyNameFour");
+        supplyThree.setSupplyDescription("SupplyDescriptionFour");
+        supplyThree.setSupplyReference("SupplyReferenceFour");
+        supplyThree.setBrandName("BrandNameFour");
+        supplyOne.setSupplyCode("SupplyCodeFour");
+        supplyOne.setSupplyUnit("SupplyUnitFour");
+        supplyOne.setAddDate(dateTimeFour);
+
     }
 
     @DisplayName("JUnit test for Given Supply Object when Register Supply then Return Supply DTO Object")
@@ -141,5 +168,57 @@ public class SupplyServiceTest {
 
         // Then/Assert
         assertNotNull(updatedSupply);
+        assertEquals("SupplyReferenceOne Changed", updatedSupply.getSupplyReference());
+        assertEquals("SupplyNameOne Changed", updatedSupply.getSupplyName());
+    }
+
+    @DisplayName("JUnit test for Given Supply Object when Update Supply with Invalid Id then Throws ResourceNotFoundException")
+    @Test
+    public void testGivenSupplyObjectWhenUpdateSupplyWithInvalidIdThenThrowsResourceNotFoundException(){
+        // Given/Arrange
+        Long validId = 1L;
+        Long invalidId = 2L;
+        supplyOne.setSupplyId(validId);
+
+        SupplyDTO supplyDTO = new SupplyDTO();
+        supplyDTO.setSupplyReference("SupplyReferenceOne");
+        supplyDTO.setSupplyName("SupplyNameOne");
+        given(supplyRepository.findById(validId)).willReturn(Optional.of(supplyOne));
+        given(supplyRepository.findById(invalidId)).willReturn(Optional.empty());
+        given(supplyRepository.findBySupplyReference(supplyDTO.getSupplyReference())).willReturn(supplyOne);
+        given(supplyRepository.save(supplyOne)).willReturn(supplyOne);
+
+        // When/Act
+        when(modelMapper.map(supplyDTO, Supply.class)).thenReturn(supplyOne);
+        when(modelMapper.map(supplyOne, SupplyDTO.class)).thenReturn(supplyDTO);
+        supplyDTO.setSupplyReference("SupplyReferenceOne Changed");
+        supplyDTO.setSupplyName("SupplyNameOne Changed");
+
+        assertThrows(ResourceNotFoundException.class, ()-> supplyService.updateSupply(2L, supplyDTO));
+
+        // Then/Assert
+        verify(supplyRepository, never()).save(any());
+    }
+
+    @DisplayName("JUnit test for Given ...")
+    @Test
+    public void testGivenSupplyObjectWhenUpdateSupplyThenReturnSupplyDTO(){
+        // Given/Arrange
+        List<Supply> mockSupplyList = List.of(
+                supplyOne,
+                supplyTwo,
+                supplyThree,
+                supplyFour
+        );
+
+        Page<Supply> supplyPage = new PageImpl(mockSupplyList, PageRequest.of(0, 1), mockSupplyList.size());
+        given(supplyRepository.findAll(any(Pageable.class))).willReturn(supplyPage);
+
+        // When/Act
+
+
+        // Then/Arrange
+
+
     }
 }
